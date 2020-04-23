816597b84dae5a00b76657c4c2ad65c78a391b0c
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-04-23 17:16:31 +0530

    Update selenium.js

--- Lecture-3_21_04_20_PepcodingAuto/raw/poc/selenium.js
+++ Lecture-3_21_04_20_PepcodingAuto/raw/poc/selenium.js
@@ -1,176 +1,181 @@
 // npm init -y
 // npm install selenium-webdriver chromedriver
-require("chromedriver");
-let fs = require("fs");
+require("chromedriver")
+
 let swd = require("selenium-webdriver");
-let credentialsFile = process.argv[2];
-let metaDataFile = process.argv[3];
-// let courseName = process.argv[4];
-// browser build
-let bldr = new swd.Builder();
-// tab 
+
+let fs = require("fs")
+
+let credentialsFile = process.argv[2]
+let username, password;
+//browser build
+let metadataFile=process.argv[3];
+let courseName=process.argv[4];
+let bldr = new swd.Builder()
+
+//tab
+
 let driver = bldr.forBrowser("chrome").build();
-// promise=> page open future 
-// value =>value 
-// pending
-// depended=> 
-// independent
-let username, password, gModules;
-// ************************************Login******************************************************
+
 let credentialsWillBeReadPromise = fs.promises.readFile(credentialsFile);
-credentialsWillBeReadPromise
-  .then(function (credentials) {
-    // buffer 
+credentialsWillBeReadPromise.then(function (credentials) {
     credentials = JSON.parse(credentials);
-    username = credentials.username
-    password = credentials.password
-    // login page
-    let googlePageWillBeOpenedPromise = driver.get("https://pepcoding.com/login");
-    return googlePageWillBeOpenedPromise;
-  })
-  .then(function () {
-    // search email,password
-    let emailWillBeSelectedPromise = driver.findElement(swd.By.css("input[type=email]"));
-    let passwordWillBeSelectedPromise = driver.findElement(swd.By.css("input[type=password]"));
-    // PromiseAll=> promiseArr=> promise=> 
-    let combinedPromise = Promise.all([emailWillBeSelectedPromise, passwordWillBeSelectedPromise]);
-    // console.log(emailWillBeSelectedPromise);
-    // array promise => answer
-    return combinedPromise;
-  }).then(function (ElementsArray) {
-    let emailWillBesendPromise = ElementsArray[0].sendKeys(username);
-    let passwordWillBeSendPromise = ElementsArray[1].sendKeys(password);
-    let combinedInputPromise = Promise.all([emailWillBesendPromise, passwordWillBeSendPromise]);
-    return combinedInputPromise;
-  }).then(function () {
-    let submitBtnWillSelected = driver.findElement(swd.By.css("button[type=submit]"));
-    return submitBtnWillSelected;
-  }).then(function (submitbtn) {
-    let submitBtnWillBeClickedPromise = submitbtn.click();
-    return submitBtnWillBeClickedPromise;
-  })
+    username = credentials.username;
+    password = credentials.password;
+    let pepWillBeOpened = driver.get("https://www.pepcoding.com/login")
+    return pepWillBeOpened;
+}).then(function () {
+    // here ".then" are aligned one after another until first ".then" finishes its promise second ".then" will not be executed.
+
+    //search email
+
+    let emailWillbeSelectedPromise = driver.findElement(swd.By.css("input[type=email")) //[] => this signify attribute.
+    let passwordWillbeSelectedPromise = driver.findElement(swd.By.css("input[type=password]"))
+    let combinePromise = Promise.all([emailWillbeSelectedPromise, passwordWillbeSelectedPromise]);
+    return combinePromise;
+
+}).then(function (Elementarr) {
+
+    //Fill emaid-field
+
+    let whatsupWillbeSendPromise = Elementarr[0].sendKeys(username);
+    let passwordWillBeenteredPromise = Elementarr[1].sendKeys(password)
+    let combinePromise = Promise.all([whatsupWillbeSendPromise, passwordWillBeenteredPromise]);
+    return combinePromise;
 
 
-  // *******************************Home Page**********************************
+}).then(function () {
 
-  .then(function () {
-    // you shouls always wait 
-    let willWaitForResourceToLoad = driver.wait(swd.until.elementLocated(swd.By.css(".resource a")));
-    return willWaitForResourceToLoad;
-  })
-  .then(function () {
-    // resource card find
-    let resourceanchorWillBeSelectedPromise = driver.findElement(swd.By.css(".resource a"))
-    // click
-    return resourceanchorWillBeSelectedPromise;
-  }).then(function (resourcePageAnchor) {
-    let rPageLinkPromise = resourcePageAnchor.getAttribute("href");
-    return rPageLinkPromise;
-    // resourcePage 
-  }).then(function (rPagelink) {
-    // console.log("Reached Courses page");
-    // console.log(rPagelink);
-    let NavigateToCourseListPage = driver.get(rPagelink);
-    return NavigateToCourseListPage;
-  })
-  .then(function () {
-    let siteOverlayWillBeSelectedPromise = driver.findElement(swd.By.css("#siteOverlay"));
-    return siteOverlayWillBeSelectedPromise;
-  })
-  .then(function (soe) {
-    let waitForOverlayToRemovePromise = driver.wait(swd.until.elementIsNotVisible(soe), 10000);
-    return waitForOverlayToRemovePromise;
-  })
-  .then(function () {
-    let courseWilBeLocatedPromise = driver.findElement(swd.By.css("#courseCard33"));
-    return courseWilBeLocatedPromise;
-  }).then(function (courseCard) {
-    let courseCardWillBeClickedPromise = courseCard.click();
-    return courseCardWillBeClickedPromise;
-  })
-  
-  // ******************************************************* Moduleclick***************************************
-  .then(function () {
-    let lisTabToBeLocatedPromise = driver.wait(swd.until.elementsLocated(swd.By.css(".lis.tab")), 10000);
-    lisTabToBeLocatedPromise
-  }).
-  then(function () {
-    let ModulesWillBeSelectedPromise = driver.findElements(swd.By.css(".lis.tab"));
-    return ModulesWillBeSelectedPromise;
-  }).then(function (modules) {
-    // console.log(modules);
-    gModules = modules
-    console.log(modules.length);
-    let moduleTextPromiseArr = [];
-    for (let i = 0; i < modules.length; i++) {
-      let moduleNamePromise = modules[i].getText();
-      moduleTextPromiseArr.push(moduleNamePromise);
-    }
-    let AllModuleNamesPromise = Promise.all(moduleTextPromiseArr);
-    return AllModuleNamesPromise;
-  }).then(function (AllModulesText) {
+    let SubmitButtonWillBeSelectedPromise = driver.findElement(swd.By.css("button[type=submit]"))
+
+    return SubmitButtonWillBeSelectedPromise
+
+}).then(function (submitElement) {
+
+    let submitButtonwillBeClicked = submitElement.click()
+
+    return submitButtonwillBeClicked
+
+}).then(function () {
+    //resouce card select & click
+    let resourceCardWillBeSelected = driver.findElement(swd.By.css(".resource a"));
+    return resourceCardWillBeSelected;
+
+}).then(function (resoucePageAnchor) {
+    let resourcePageLink = resoucePageAnchor.getAttribute("href");
+    return resourcePageLink;
+}).then(function (resourcelink) {
+    let navigateResourceLink = driver.get(resourcelink);
+    return navigateResourceLink;
+}).then(function () {
+    //resouce card select & click
+    let siteOverlayElement = driver.findElement(swd.By.css("#siteOverlay"));
+    return siteOverlayElement;
+}).then(function (soe) {
+    //resource page consist an overlay element for which we have to wait.
+    let wait = driver.wait(swd.until.elementIsNotVisible(soe), 1500);
+    return wait;
+}).then(function () {
+    let courseWillBeLocated=driver.findElement(swd.By.css("#courseCard40"));
+    return courseWillBeLocated;
+}).then(function(courseLink){
+    let navigateCourseLink = courseLink.click();
+    return navigateCourseLink;
+}).then(function(){
+    let lisTabToBeLocatedPromise=driver.wait(swd.until.elementsLocated(swd.By.css(".lis.tab")), 10000);
+    return lisTabToBeLocatedPromise;
+
+}).then(function(){
+let ModuleWillbe=driver.findElements(swd.By.css(".lis.tab"));
+return ModuleWillbe;
+}).then(function(modules){
+gModules=modules;
+console.log(modules.length);
+let moduleTextPromiseArr=[];
+for(let i=0;i<modules.length;i++){
+    let modulesNamePromise=modules[i].getText();
+    moduleTextPromiseArr.push(modulesNamePromise);
+}
+let allmodulespromise=Promise.all(moduleTextPromiseArr);
+return allmodulespromise;
+}).then(function (AllModulesText) {
     let i;
     for (i = 0; i < AllModulesText.length; i++) {
-      if (AllModulesText[i].includes("Dynamic Programming") == true) {
+      if (AllModulesText[i].includes("Recursion") == true) {
         break;
       }
     }
     let moduleWillBeclickedPromise = gModules[i].click();
     return moduleWillBeclickedPromise;
-  })
-  // ********************************************************Lecture**************************************
-  // *******************************************************Question**************************************
-.then(function () {
-  //   read json file 
-  let metaDataWillBeReadPromise = fs.promises.readFile(metaDataFile);
-  return metaDataWillBeReadPromise;
-})
-  .then(function (metadata) {
-    metadata = JSON.parse(metadata);
-    let question = metadata[0];
-    //   let WillOpenQuestionPagePromise = driver.get(question.url);
-    //   return WillOpenQuestionPagePromise;
-    // }).then(function () {
-    //   console.log("Opened Question page");
-    // })
-    // list=> names ,
-    // name => medata.json => question name
-  }).catch(function (err) {
-    console.log(err);
-  })
-
-
-
-function goToQuestionPage() {
-
-  let lisTabToBeLocatedPromise = driver.wait(swd.until.elementsLocated(swd.By.css(".lis.tab")), 10000);
-  lisTabToBeLocatedPromise.
-
-    then(function () {
-      let ModulesWillBeSelectedPromise = driver.findElements(swd.By.css(".lis.tab"));
-      return ModulesWillBeSelectedPromise;
-    }).then(function (modules) {
-      // console.log(modules);
-      gModules = modules
-      console.log(modules.length);
-      let moduleTextPromiseArr = [];
-      for (let i = 0; i < modules.length; i++) {
-        let moduleNamePromise = modules[i].getText();
-        moduleTextPromiseArr.push(moduleNamePromise);
+  }).then(function(){
+    let LectureWillbePromised=driver.findElements(swd.By.css("#module2 p"));
+    return LectureWillbePromised;
+  }).then(function(lectures){
+      console.log(lectures.length);
+      glectures=lectures;
+      let lecturesPromiseArr=[];
+      for(let i=0;i<lectures.length;i++){
+          lecturesPromiseArr.push(lectures[i].getText());
       }
-      let AllModuleNamesPromise = Promise.all(moduleTextPromiseArr);
-      return AllModuleNamesPromise;
-    }).then(function (AllModulesText) {
-      let i;
-      for (i = 0; i < AllModulesText.length; i++) {
-        if (AllModulesText[i].includes("Dynamic Programming") == true) {
+      let alllecturesPromise=Promise.all(lecturesPromiseArr);
+      return alllecturesPromise;
+      
+  }).then(function(lectures){
+    // console.log(lectures);
+    //let metad=metadataFile
+    let i=0;
+    for(i=0;i<lectures.length;i++){
+        if (lectures[i].includes("Recursion in Arrays") == true) {
+            break;
+          }
+    }
+     let LectureWillbeClickedPromise=glectures[i].click();
+     return LectureWillbeClickedPromise;
+  }).then(function(){
+      let questionWillBeSelectedpromise=driver.findElements(swd.By.css(".collection.resourceList li p"));
+      return questionWillBeSelectedpromise;
+  }).then(function(questions){
+  //  console.log(questions.length);
+    gquestions=questions;
+    let questionsPromiseArr=[];
+    for(let i=0;i<questions.length;i++){
+        questionsPromiseArr.push(questions[i].getText());
+    }
+    let allquestionsPromise=Promise.all(questionsPromiseArr);
+    return allquestionsPromise;
+    
+}).then(function(questions){
+  // console.log(lectures);
+  //let metad=metadataFile
+  let i=0;
+  for(i=0;i<questions.length;i++){
+      if (questions[i].includes("First Index") == true) {
           break;
         }
-      }
-      let moduleWillBeclickedPromise = gModules[i].click();
-      return moduleWillBeclickedPromise;
-    })
-}
+  }
+   let questionWillbeClickedPromise=gquestions[i].click();
+   return questionWillbeClickedPromise
+}).then(function(){
+    let editorWillBeSelectedpromise=driver.findElements(swd.By.css(".tab.bold.editorTab"));
+    return editorWillBeSelectedpromise;
+    
+}).then(function(Editors){
+    // console.log(lectures);
+    //let metad=metadataFile
+    
+     let EditorWillbeClickedPromise=Editors[0].click();
+     return EditorWillbeClickedPromise
+  }).then(function(){
+      console.log("Editor clicked..");
+      
+  })
+.catch(function (err) {
+
+    console.log(err);
+
+
+
+})
 
 
 
